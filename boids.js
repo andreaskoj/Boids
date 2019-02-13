112aa9feefe4a4df990914eac81bff32b77b80d5
Andreas Koj <kojandreas@gmail.com>
2019-02-13 22:29:10 +0100

    working on modeling the entity and movement

--- boids.js
+++ boids.js
@@ -1,37 +1,24 @@
-/*jslint browser: true */
-/*global window */
-/*global console */
-
-
 var canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 c = canvas.getContext('2d')
 
-//c.fillStyle = '#439f20'
-//c.fillRect(100, 100,100, 100)
-//
-//// Line
-//c.beginPath();
-//c.moveTo(50,300);
-//c.lineTo(300,100);
-//c.lineTo(400,200);
-//c.strokeStyle = "red";
-//c.stroke();
-//
-////Arc
-//c.beginPath();
-//c.arc(Math.random() *window.innerWidth,400,30,0, Math.PI*2, false);
-//c.stroke();
+//Global variables
 
-window.addEventListener('click', function() {
-    console.log("ok");
-    
-})
+var entities = [];
 
-c.beginPath();
-c.arc(200,400,30,0, Math.PI*2, false);
-c.stroke();
+
+
+//c.beginPath();
+//c.moveTo(300,300);
+//c.lineTo(285,250);
+//c.lineTo(270,300);
+//c.closePath();
+//c.fill();
+//
+
+
+
 
 var x = Math.random() * innerWidth;
 var y = Math.random() * innerHeight;
@@ -39,10 +26,61 @@ var dx = (Math.random() - 0.5 ) *20;
 var dy = (Math.random() - 0.5 ) *20;
 var radious = 30;
 
-function Bird() {
-    
+class Bird {
+            
+    constructor(pos_x, pos_y, dx, dy) {
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
         
+        this.dx = dx;
+        this.dy = dy;
+
+        this.vertexLeftX = pos_x
+        this.vertexLeftY = pos_y
+        
+        this.vertexTopX = this.vertexLeftX + -20;
+        this.vertexTopY = this.vertexLeftY + -60;
+        
+        this.vertexRightX = this.vertexTopX + -20;
+        this.vertexRightY = this.vertexTopY + 60;
+        console.log(this.vertexTopX);
+    }
+    
+    draw() {
+        //console.log(this.vertexLeftX);
+        c.beginPath();
+        c.moveTo(this.vertexLeftX, this.vertexLeftY);
+        c.lineTo(this.vertexTopX, this.vertexTopY);
+        c.lineTo(this.vertexRightX, this.vertexRightY);
+        c.closePath();
+        c.fill();
+    }
+    
+    update() {
+        console.log(this.vertexTopX);
+        this.vertexTopX = this.vertexTopX - this.dx;
+        this.vertexTopY = this.vertexTopY - this.dy;
+        
+        this.vertexLeftX = this.vertexLeftX - this.dx;
+        this.vertexLeftY = this.vertexLeftY - this.dy;
+        
+        this.vertexRightX = this.vertexRightX - this.dx;
+        this.vertexRightY = this.vertexRightY - this.dy;
+        
+        this.draw;
+    }
+    
+    
 }
+var b1 = new Bird(200,200,50,50);    
+
+
+
+b1.draw();
+
+b1.update();
+b1.update();
+b1.update();
 
 function Circle(x,y,dx,dy) {
     this.x = x;
@@ -73,17 +111,32 @@ function Circle(x,y,dx,dy) {
     }
 }
 
-var circle = new Circle(200,200, 10, 10);
 
 
 
-function animate() {
-    requestAnimationFrame(animate);
-    //clears the canvas
-    c.clearRect(0,0,innerWidth,innerHeight);
+entities.push(new Circle(200,200, 10, 10));
+entities.push(new Circle(00,200, 10, 10));
+
+
+//function animate() {
+//    requestAnimationFrame(animate);
+//    
+//    c.clearRect(0,0,innerWidth,innerHeight);
+//    
+//    //b1.update();
+////    
+////    entities.forEach(function(element){
+////        element.update();    
+////    });
+////    
+//    
+//}
+
+//animate();
+
+window.addEventListener('click', function(event) {
     
-    circle.update();
+    entities.push(new Circle(event.x,event.y, 10, 10));
+    console.log(entities.length);
     
-}
-
-animate();
+})
